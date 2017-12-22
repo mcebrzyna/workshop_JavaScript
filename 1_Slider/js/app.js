d76c3e405e186710da7e34ca59026a49955b2ff8
Michal Cebrzyna <mcebrzyna@gmail.com>
2017-12-22 13:13:23 +0100

    Slider

--- 1_Slider/js/app.js
+++ 1_Slider/js/app.js
@@ -1,3 +1,23 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener("DOMContentLoaded", function () {
+
+    var nextBtn = document.querySelector('#nextPicture');
+    var prevBtn = document.querySelector('#prevPicture');
+    var imgList = document.querySelectorAll('.slider li');
+    var index = 0;
+
+    imgList[index].classList.add('visible');
+
+    nextBtn.addEventListener('click', function (event) {
+
+        imgList[index].classList.remove('visible');
+        (index === imgList.length -1) ? index = 0 : index++;
+        imgList[index].classList.add('visible');
+    });
+
+    prevBtn.addEventListener('click', function (event) {
+
+        imgList[index].classList.remove('visible');
+        (index === 0) ? index = imgList.length -1 : index--;
+        imgList[index].classList.add('visible');
+    });
+});
