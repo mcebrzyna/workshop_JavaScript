c5c0ca9b01f1b9e1eb5a6319d0e306f771c7d344
Michal Cebrzyna <mcebrzyna@gmail.com>
2017-12-24 09:16:32 +0100

    add interval to slider

--- 1_Slider/js/app.js
+++ 1_Slider/js/app.js
@@ -4,20 +4,33 @@ document.addEventListener("DOMContentLoaded", function () {
     var prevBtn = document.querySelector('#prevPicture');
     var imgList = document.querySelectorAll('.slider li');
     var index = 0;
+    var lastChange = 0;
 
-    imgList[index].classList.add('visible');
-
-    nextBtn.addEventListener('click', function (event) {
-
+    function nextImg() {
         imgList[index].classList.remove('visible');
         (index === imgList.length -1) ? index = 0 : index++;
         imgList[index].classList.add('visible');
-    });
+        lastChange = 0;
+    }
 
-    prevBtn.addEventListener('click', function (event) {
-
+    function prevImg() {
         imgList[index].classList.remove('visible');
         (index === 0) ? index = imgList.length -1 : index--;
         imgList[index].classList.add('visible');
-    });
+        lastChange = 0;
+    }
+
+    imgList[index].classList.add('visible');
+
+    //SLIDER
+
+    nextBtn.addEventListener('click', nextImg);
+    prevBtn.addEventListener('click', prevImg);
+
+    setInterval(function () {
+        if (lastChange === 7 /*amount of seconds between slide*/){
+            return nextImg();
+        }
+        lastChange += 1;
+    }, 1000);
 });
