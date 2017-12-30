c5f48d7479fd9f1579ce5aa76fd724b313a82e23
Michal Cebrzyna <mcebrzyna@gmail.com>
2017-12-30 16:41:44 +0100

    Gallery with tag filter

--- 3_GalleryWithFilter/js/app.js
+++ 3_GalleryWithFilter/js/app.js
@@ -1,3 +1,31 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener('DOMContentLoaded', function () {
+
+    var images = document.querySelectorAll('img');
+    var hideButton = document.querySelector('#hideButton');
+    var showButton = document.querySelector('#showButton');
+    var tagInput = document.querySelector('#tagInput');
+
+    showButton.addEventListener('click', function (event) {
+
+        images.forEach(function (image) {
+           if(image.dataset.tag.indexOf(tagInput.value) === -1){
+               image.style.display = 'none';
+           }
+           else{
+               image.style.display = 'inline-block';
+           }
+        });
+    });
+
+    hideButton.addEventListener('click', function () {
+        images.forEach(function (image) {
+            if(image.dataset.tag.indexOf(tagInput.value) !== -1){
+                image.style.display = 'none';
+            }
+            else{
+                image.style.display = 'inline-block';
+            }
+        });
+    });
+});
+
