a86e714c045bf71d997cec82468e3a70458ff1f6
Michal Cebrzyna <mcebrzyna@gmail.com>
2017-12-30 16:39:52 +0100

    Gallery

--- 2_Gallery/js/app.js
+++ 2_Gallery/js/app.js
@@ -1,10 +1,26 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener('DOMContentLoaded', function () {
 
-/*
- <div class="fullScreen">
-   <img src="./images/wrong.png">
-   <button class="close">Close</button>
- </div>
- */
+    var images = document.querySelectorAll('.gallery img');
+    var body = document.querySelector('body');
+
+    images.forEach(function (image) {
+        image.addEventListener('click', function (event) {
+            var fullScreenDiv = document.createElement('div');
+            fullScreenDiv.classList.add('fullScreen');
+            body.appendChild(fullScreenDiv);
+
+            var fullScrenImg = document.createElement('img');
+            fullScrenImg.setAttribute('src',this.getAttribute('src'));
+            fullScreenDiv.appendChild(fullScrenImg);
+
+            var fullScreenButton = document.createElement('button');
+            fullScreenButton.classList.add('close');
+            fullScreenButton.innerText = 'close';
+            fullScreenDiv.appendChild(fullScreenButton);
+
+            fullScreenButton.addEventListener('click', function (event) {
+                fullScreenDiv.remove();
+            });
+        });
+    });
+});
