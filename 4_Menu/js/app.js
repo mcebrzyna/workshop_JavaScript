b6b97cebbea599ec523b2c74370899c5c163b44f
Michal Cebrzyna <mcebrzyna@gmail.com>
2018-01-04 22:28:17 +0100

    Menu

--- 4_Menu/js/app.js
+++ 4_Menu/js/app.js
@@ -1,3 +1,18 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener('DOMContentLoaded', function () {
+
+    var menuElements = document.querySelectorAll('.menu > li');
+
+    menuElements.forEach(function (element) {
+
+        var submenu = element.querySelector('.submenu');
+
+        if(submenu != null){
+            element.addEventListener('mouseover', function (event) {
+                submenu.style.display = 'block';
+            });
+            element.addEventListener('mouseout', function (event) {
+                submenu.style.display = 'none';
+            });
+        }
+    });
+});
